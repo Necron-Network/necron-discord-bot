9e6e7f141bc2d37a578daf8be2cbd7763d38d406
Mednoob <ahmadyaqdhan123@gmail.com>
2021-05-26 11:52:34 +0900

    feat(*): ?

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { ClientOptions, ClientPresenceStatus, Intents, UserResolvable } from "discord.js";
 
-export const defaultPrefix = "$";
-export const devs: UserResolvable[] = ["290159952784392202"]; // NOTE: Please change this
+export const defaultPrefix = ";";
+export const devs: UserResolvable[] = ["366169273485361153"]; // NOTE: Please change this
 export const clientOptions: ClientOptions = {               // https://discord.js.org/#/docs/main/stable/typedef/ClientOptions
     allowedMentions: { parse: ["users"] },    // NOTE: Please configure these after you're using this template,
     fetchAllMembers: false,                 // especially allowedMentions, fetchAllMembers, messageCacheLifetime, messageSweepInterval, and Intents
@@ -17,7 +17,7 @@ export const clientOptions: ClientOptions = {               // https://discord.j
 };
 export const isProd = process.env.NODE_ENV === "production";
 export const isDev = !isProd;
-export const prefix = isDev ? "d$" : "$";
+export const prefix = isDev ? "d;" : ";";
 export const presenceData = {
     activities: [
         "Hello, World!",
@@ -30,4 +30,4 @@ export const presenceData = {
     interval: 60000
 };
 export const shardsCount: number | "auto" = "auto";
-export const dbName = "";
+export const dbName = "NecronDatabase";
