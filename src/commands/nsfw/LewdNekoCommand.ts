ae5d84bc3d438178cf211d319a7d278359f8d6d6
Mednoob <ahmadyaqdhan123@gmail.com>
2021-06-02 22:31:27 +0900

    feat(commands): add NSFW category, add LewdNekoCommand

---
+++ src/commands/nsfw/LewdNekoCommand.ts
@@ -0,0 +1,28 @@
+import { BaseCommand } from "../../structures/BaseCommand";
+import { IMessage, ITextChannel, INekosLifeImgResponse } from "../../typings";
+import { DefineCommand } from "../../utils/decorators/DefineCommand";
+import { createEmbed } from "../../utils/createEmbed";
+
+@DefineCommand({
+    description: "Gives you a random lewd neko image",
+    name: "lewdneko",
+    usage: "{prefix}lewdneko"
+})
+export class LewdNekoCommand extends BaseCommand {
+    public async execute(message: IMessage): Promise<any> {
+        if (!(message.channel as ITextChannel).nsfw) {
+            return message.channel.send(createEmbed("error", "You can't use this command outside NSFW channel")).catch(() => undefined).then(m => {
+                if (!m) return;
+
+                setTimeout(() => {
+                    m.delete().catch(() => null);
+                }, 5000);
+            });
+        }
+
+        const { url } = await this.client.request.get("https://nekos.life/api/v2/img/lewd").json<INekosLifeImgResponse>();
+
+        return message.channel.send(createEmbed("success").setTitle("Lewd Neko Image").setURL(url)
+            .setImage(url));
+    }
+}
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -113,3 +113,7 @@ export interface ISong {
     thumbnail: string;
     duration: number;
 }
+
+export interface INekosLifeImgResponse {
+    url: string;
+}
