d87057d72e39662c819abd7aaa0bf70224dca0e7
Mednoob <ahmadyaqdhan123@gmail.com>
2021-06-08 13:42:10 +0900

    feat(commands): add SupportCommand

---
+++ src/commands/general/SupportCommand.ts
@@ -0,0 +1,15 @@
+import { BaseCommand } from "../../structures/BaseCommand";
+import { IMessage } from "../../typings";
+import { DefineCommand } from "../../utils/decorators/DefineCommand";
+import { createEmbed } from "../../utils/createEmbed";
+
+@DefineCommand({
+    description: "Show support server of this bot",
+    name: "support",
+    usage: "{prefix}usage"
+})
+export class SupportCommand extends BaseCommand {
+    public execute(message: IMessage): any {
+        return message.channel.send(createEmbed("info", "You want to join our support server? **[Click Here](https://discord.com/invite/stfp26mhr4)**"));
+    }
+}
