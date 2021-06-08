5803a92148bab0990b32e2709c7a85359d2253f2
Mednoob <ahmadyaqdhan123@gmail.com>
2021-06-08 12:29:58 +0900

    feat(IpCommand): add server status

--- src/commands/general/IpCommand.ts
+++ src/commands/general/IpCommand.ts
@@ -2,15 +2,33 @@ import { BaseCommand } from "../../structures/BaseCommand";
 import { IMessage } from "../../typings";
 import { DefineCommand } from "../../utils/decorators/DefineCommand";
 import { createEmbed } from "../../utils/createEmbed";
+import { status } from "minecraft-server-util";
 
 @DefineCommand({
     name: "ip",
     usage: "{prefix}ip"
 })
 export class IpCommand extends BaseCommand {
-    public execute(message: IMessage): any {
-        if (message.guild?.id !== "735020665861832756") return;
+    public async execute(message: IMessage): Promise<any> {
+        const serverStatus = await status("necron-network.com").catch(() => undefined);
+        if (!serverStatus) return;
 
-        return message.channel.send(createEmbed("info", "necron-network.com"));
+        return message.channel.send(createEmbed("info").addFields([
+            {
+                name: "IP Address",
+                value: `${serverStatus.host}:${serverStatus.port}`,
+                inline: true
+            },
+            {
+                name: "Players",
+                value: `${serverStatus.onlinePlayers ?? "?"}/${serverStatus.maxPlayers ?? "?"}`,
+                inline: true
+            },
+            {
+                name: "MOTD",
+                value: `${serverStatus.description?.toString() ?? "No Information"}`,
+                inline: false
+            }
+        ]).setThumbnail(serverStatus.favicon ?? ""));
     }
 }
