94b2cc7fbf584270c00f007f69cb8e809166feed
Mednoob <ahmadyaqdhan123@gmail.com>
2021-07-01 00:07:30 +0900

    feat(DevelopersCommand): Change embed description

--- src/commands/general/DevelopersCommand.ts
+++ src/commands/general/DevelopersCommand.ts
@@ -13,6 +13,6 @@ export class DevelopersCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<any> {
         const devs = await Promise.all(this.client.config.devs.map(x => this.client.utils.fetchUser(x as string)));
 
-        return message.channel.send(createEmbed("info", `Here are the list of developers that have worked hard on improving me time to time: ${devs.filter(x => x !== undefined).map(x => `\`${x!.tag}\``).join(", ")}\n\nWant to contribute this project? You can join our support server and contact one of our developers.`));
+        return message.channel.send(createEmbed("info", `Here are the list of developers that have worked hard on improving me time to time: ${devs.filter(x => x !== undefined).map(x => `\`${x!.tag}\``).join(", ")}\n\nWant to contribute this project? You can contact one of my developers.`));
     }
 }
