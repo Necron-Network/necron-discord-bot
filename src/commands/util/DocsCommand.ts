51df311dcd56ce04cfe77db5c7a685ae4922a5d6
Mednoob <ahmadyaqdhan123@gmail.com>
2021-06-30 13:26:06 +0000

    feat(DocsCommand): Change search result stylings

--- src/commands/util/DocsCommand.ts
+++ src/commands/util/DocsCommand.ts
@@ -2,6 +2,7 @@ import { IMessage } from "../../typings";
 import { BaseCommand } from "../../structures/BaseCommand";
 import { DefineCommand } from "../../utils/decorators/DefineCommand";
 import { createEmbed } from "../../utils/createEmbed";
+import { MessageEmbedOptions, MessageEmbed } from "discord.js";
 
 @DefineCommand({
     aliases: ["documentation", "discord.js", "discordjs", "djs", "djsdocs"],
@@ -22,7 +23,24 @@ export class DocsCommand extends BaseCommand {
             if (source === "11.5.1" || source === "11.6.4") {
                 source = `https://raw.githubusercontent.com/discordjs/discord.js/docs/${source}.json`;
             }
-            const embed = await this.client.request.get("https://djsdocs.sorta.moe/v2/embed", { searchParams: { src: source, q: query } }).json();
+            let embed: MessageEmbedOptions|MessageEmbed = await this.client.request.get("https://djsdocs.sorta.moe/v2/embed", { searchParams: { src: source, q: query } }).json<MessageEmbedOptions>();
+            if (embed.title === "Search results:") {
+                embed = new MessageEmbed(embed);
+                const results = embed.description!.split("\n");
+                const properties = results.filter(x => x.startsWith(":regional_indicator_p:"));
+                const methods = results.filter(x => x.startsWith(":regional_indicator_m:"));
+                const typedefs = results.filter(x => x.startsWith(":regional_indicator_t:"));
+                const classes = results.filter(x => x.startsWith(":regional_indicator_c:"));
+                const events = results.filter(x => x.startsWith(":regional_indicator_e:"));
+
+                if (classes.length) embed.addField("Classes", classes.map(x => x.split(" ").slice(1).join(" ")).join("\n"), true);
+                if (properties.length) embed.addField("Properties", properties.map(x => x.split(" ").slice(1).join(" ")).join("\n"), true);
+                if (methods.length) embed.addField("Methods", methods.map(x => x.split(" ").slice(1).join(" ")).join("\n"), true);
+                if (events.length) embed.addField("Events", events.map(x => x.split(" ").slice(1).join(" ")).join("\n"), true);
+                if (typedefs.length) embed.addField("Typedefs", typedefs.map(x => x.split(" ").slice(1).join(" ")).join("\n"), true);
+
+                embed.description = "";
+            }
             return message.channel.send({ embed });
         } catch (err) {
             return message.channel.send(createEmbed("error", `\`${err.message}\``));
