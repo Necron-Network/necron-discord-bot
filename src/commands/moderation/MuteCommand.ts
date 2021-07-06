35f6bcb6cf3b82fd08e2b0faa302513c44ab25f9
Mednoob <ahmadyaqdhan123@gmail.com>
2021-07-06 22:02:07 +0900

    feat(MuteCommand): add isNewRole check

--- src/commands/moderation/MuteCommand.ts
+++ src/commands/moderation/MuteCommand.ts
@@ -18,6 +18,7 @@ export class MuteCommand extends BaseCommand {
 
         const collection = this.client.db?.collection<IMuteRole>("muterole");
         const roleData = await collection?.findOne({ guildID: message.guild.id });
+        let isNewRole = false;
         let role = message.guild.roles.cache.get(roleData?.roleID as string);
         if (!role) {
             if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send(createEmbed("error", "I don't have `Manage Channels` permission to set role permission on channels!"));
@@ -40,6 +41,7 @@ export class MuteCommand extends BaseCommand {
             };
             roleData ? await collection?.updateOne({ guildID: message.guild.id }, { $set: newData }, { upsert: true }) : await collection?.insertOne(newData);
             role = newRole;
+            isNewRole = true;
         }
 
         const result = await member.roles.add(role.id).catch(() => undefined);
@@ -47,12 +49,14 @@ export class MuteCommand extends BaseCommand {
 
         await message.channel.send(createEmbed("success", "Successfully muted").setAuthor(member.user.tag, member.user.displayAvatarURL({ format: "png", size: 2048, dynamic: true })));
 
-        for (const channel of message.guild.channels.cache.array()) {
-            await channel.createOverwrite(role.id, {
-                SEND_MESSAGES: false,
-                SEND_TTS_MESSAGES: false,
-                ADD_REACTIONS: false
-            }).catch(() => undefined);
+        if (isNewRole) {
+            for (const channel of message.guild.channels.cache.array()) {
+                await channel.createOverwrite(role.id, {
+                    SEND_MESSAGES: false,
+                    SEND_TTS_MESSAGES: false,
+                    ADD_REACTIONS: false
+                }).catch(() => undefined);
+            }
         }
     }
 }
