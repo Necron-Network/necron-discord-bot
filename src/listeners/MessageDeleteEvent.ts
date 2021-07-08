a6db270b1026afdf6c13dbe0b4eed91558f462e4
Mednoob <ahmadyaqdhan123@gmail.com>
2021-07-08 21:55:47 +0900

    feat(MessageDeleteEvent): send logs

--- src/listeners/MessageDeleteEvent.ts
+++ src/listeners/MessageDeleteEvent.ts
@@ -9,6 +9,8 @@ export class MessageDeleteEvent extends BaseListener {
     public async execute(message: IMessage): Promise<any> {
         if (message.partial as boolean || message.channel.type !== "text") return;
 
+        void this.client.logs.send(message.guild!.id, "message-delete", message).catch(() => null);
+
         const mentions: (User|Role)[] = [];
         mentions.push(...message.mentions.users.values());
         mentions.push(...message.mentions.roles.values());
@@ -17,6 +19,8 @@ export class MessageDeleteEvent extends BaseListener {
         const data = await this.client.db?.collection<IGhostPingReminder>("ghostpingreminder").findOne({ guild: message.guild!.id });
         if (!data?.enabled) return;
         if (data.whitelistedChannels.includes(message.channel.id) || data.whitelistedUsers.includes(message.author.id)) return;
+
+        void this.client.logs.send(message.guild!.id, "ghost-ping", message).catch(() => null);
 
         return message.channel.send(createEmbed("info", message.content).setAuthor(message.author.tag, message.author.displayAvatarURL({ format: "png", size: 2048, dynamic: true })).setTitle("Ghost Ping Reminder")
             .addField("Mentions", mentions.map(x => x.toString()).join(" ")));
