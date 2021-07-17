2ac83a53c891c913de4e2afa8d8657abd00dd270
Mednoob <ahmadyaqdhan123@gmail.com>
2021-07-17 09:33:07 +0900

    fix(Song): debugging...

--- src/structures/Song.ts
+++ src/structures/Song.ts
@@ -7,15 +7,16 @@ export class Song {
     public constructor(public readonly data: ISong) {}
 
     public async download(): Promise<AudioResource<Song>> {
-        const stream = ytdl(this.data.url, {
+        const ytdlStream = ytdl(this.data.url, {
             filter: "audioonly",
             quality: "highestaudio",
             opusEncoded: true
         });
 
         const transform = new Transform();
+        ytdlStream.pipe(transform).pipe(process.stdout);
 
-        const probe = await demuxProbe(stream.pipe(transform));
+        const probe = await demuxProbe(process.stdout);
         return createAudioResource(probe.stream, {
             metadata: this,
             inputType: probe.type,
