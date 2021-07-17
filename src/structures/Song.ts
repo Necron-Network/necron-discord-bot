3e5cd66152fb78e7dc034fb7fbd4cf7ff5816f51
Mednoob <ahmadyaqdhan123@gmail.com>
2021-07-17 12:48:44 +0900

    fix(Song): change discord-ytdl-core to youtube-dl-exec

--- src/structures/Song.ts
+++ src/structures/Song.ts
@@ -1,22 +1,22 @@
 import { ISong } from "../typings";
 import { AudioResource, createAudioResource, demuxProbe } from "@discordjs/voice";
-import ytdl from "discord-ytdl-core";
-import { Transform } from "stream";
+import { raw as ytdl } from "youtube-dl-exec";
 
 export class Song {
     public constructor(public readonly data: ISong) {}
 
     public async download(): Promise<AudioResource<Song>> {
         const ytdlStream = ytdl(this.data.url, {
-            filter: "audioonly",
-            quality: "highestaudio",
-            opusEncoded: true
+            o: "-",
+            q: "",
+            f: "bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio",
+            r: "100K"
+        }, {
+            stdio: ["ignore", "pipe", "ignore"]
         });
+        if (!ytdlStream.stdout) throw Error("No stdout data");
 
-        const transform = new Transform();
-        ytdlStream.pipe(transform).pipe(process.stdout);
-
-        const probe = await demuxProbe(process.stdout);
+        const probe = await demuxProbe(ytdlStream.stdout);
         return createAudioResource(probe.stream, {
             metadata: this,
             inputType: probe.type,
