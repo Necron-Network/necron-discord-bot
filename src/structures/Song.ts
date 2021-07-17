8b95f2baf6f034101bc5e2e2c5abc7fd1554a251
Mednoob <ahmadyaqdhan123@gmail.com>
2021-07-17 14:19:39 +0900

    feat(Song): debugging...

--- src/structures/Song.ts
+++ src/structures/Song.ts
@@ -5,22 +5,25 @@ import { raw as ytdl } from "youtube-dl-exec";
 export class Song {
     public constructor(public readonly data: ISong) {}
 
-    public async download(): Promise<AudioResource<Song>> {
-        const ytdlStream = ytdl(this.data.url, {
-            o: "-",
-            q: "",
-            f: "bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio",
-            r: "100K"
-        }, {
-            stdio: ["ignore", "pipe", "ignore"]
-        });
-        if (!ytdlStream.stdout) throw Error("No stdout data");
+    public download(): Promise<AudioResource<Song>> {
+        return new Promise((resolve, reject) => {
+            const ytdlStream = ytdl(this.data.url, {
+                o: "-",
+                q: "",
+                f: "bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio",
+                r: "100K"
+            }, {
+                stdio: ["ignore", "pipe", "ignore"]
+            });
+            if (!ytdlStream.stdout) throw Error("No stdout data");
 
-        const probe = await demuxProbe(ytdlStream.stdout);
-        return createAudioResource(probe.stream, {
-            metadata: this,
-            inputType: probe.type,
-            inlineVolume: true
+            void ytdlStream.on("spawn", () => {
+                void demuxProbe(ytdlStream.stdout!).then(probe => resolve(createAudioResource(probe.stream, { metadata: this, inputType: probe.type, inlineVolume: true }))).catch(err => {
+                    if (!ytdlStream.killed) ytdlStream.kill();
+                    ytdlStream.stdout?.resume();
+                    reject(err);
+                });
+            });
         });
     }
 }
